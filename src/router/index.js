a66a234608c340ded6c31e3992ccb929e21a514c
yjovo <877728213@qq.com>
2018-04-22 09:52:23 +0800

    添加路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,57 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
-import Home from '@/components/page/Home'
+import Home from '@/components/Home'
+import Cart from '@/components/Cart'
+import Login from '@/components/Login'
+import Publish from '@/components/Publish'
+import Searchresult from '@/components/Searchresult'
+import Shop from '@/components/Shop'
+import Shopitem from '@/components/Shopitem'
+import Usercenter from '@/components/Usercenter'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    },
-
-    {
-      path: '/',
-      name: 'Home',
+      path: '/home',
       component: Home
+    },
+    {
+      path:'/shop',
+      component:Shop
+    },
+    {
+      path:'/searchresult',
+      component:Searchresult
+    },
+    {
+      path:'/shopitem',
+      component:Shopitem
+    },
+    {
+      path:'/usercenter',
+      component:Usercenter
+    },
+    {
+      path:'/publish',
+      component:Publish
+    },
+    {
+      path:'/cart',
+      component:Cart
+    },
+    {
+      path:'/login',
+      component:Login
+    },
+    {
+      path:'/',
+      redirect:'/home'
+    },
+    {
+      path:'/*',
+      redirect:'/home'
     }
   ]
 })
