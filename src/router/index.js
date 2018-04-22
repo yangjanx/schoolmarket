afcd606e778cebfc6839d31c08fe4b9da64afb3c
yjovo <877728213@qq.com>
2018-04-22 18:17:07 +0800

    添加HomeFirst路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/components/Home'
+import HomeFirst from '@/components/HomeFirst'
 import Cart from '@/components/Cart'
 import Login from '@/components/Login'
 import Publish from '@/components/Publish'
@@ -15,43 +16,46 @@ export default new Router({
   routes: [
     {
       path: '/home',
-      component: Home
+      component: Home,
+      children: [
+        { path: '/', component: HomeFirst }
+      ]
     },
     {
-      path:'/shop',
-      component:Shop
+      path: '/shop',
+      component: Shop
     },
     {
-      path:'/searchresult',
-      component:Searchresult
+      path: '/searchresult',
+      component: Searchresult
     },
     {
-      path:'/shopitem',
-      component:Shopitem
+      path: '/shopitem',
+      component: Shopitem
     },
     {
-      path:'/usercenter',
-      component:Usercenter
+      path: '/usercenter',
+      component: Usercenter
     },
     {
-      path:'/publish',
-      component:Publish
+      path: '/publish',
+      component: Publish
     },
     {
-      path:'/cart',
-      component:Cart
+      path: '/cart',
+      component: Cart
     },
     {
-      path:'/login',
-      component:Login
+      path: '/login',
+      component: Login
     },
     {
-      path:'/',
-      redirect:'/home'
+      path: '/',
+      redirect: '/home'
     },
     {
-      path:'/*',
-      redirect:'/home'
+      path: '/*',
+      redirect: '/home'
     }
   ]
 })
